ebc2a5a8d30ace9c69b8a37e70dce2e5bb98d529
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-06-09 18:31:30 +0200

    improve basic layout for categories

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,12 +1,22 @@
 <template>
     <div id="main-page">
-        bloup
+        <div class="categories-wrapper">
+            <recipe-category></recipe-category>
+            <recipe-category></recipe-category>
+            <recipe-category></recipe-category>
+            <recipe-category></recipe-category>
+        </div>
     </div>
 </template>
 
 <script>
+import RecipeCategory from './RecipeCategory/RecipeCategory.vue';
+
 export default {
     name: "MainPage",
+    components: {
+        RecipeCategory,
+    }
 }
 </script>
 
@@ -14,7 +24,32 @@ export default {
 #main-page {
     height: 100%;
     width: 100%;
-    background-color: #F5F5F5;
+    overflow: scroll;
+    background-color: #E0E0E0;
+}
+.categories-wrapper {
+    padding: 10px 20px;
+    display: grid;
+    gap: 20px;
+    grid-template-columns: auto auto auto auto;
+}
+
+@media only screen and (max-width: 1700px) {
+    .categories-wrapper {
+        grid-template-columns: auto auto auto;
+    }
+
+@media only screen and (max-width: 1300px) {
+    .categories-wrapper {
+        grid-template-columns: auto auto;
+    }
+}
+
+@media only screen and (max-width: 900px) {
+    .categories-wrapper {
+        grid-template-columns: auto;
+    }
+}
 }
 </style>
 
---
+++ src/components/RecipeCategory/RecipeCategory.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="category-card-wrapper">
+        <div class="category-card-content">
+            <div class="category-card-name">
+                Category
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.category-card-wrapper {
+    border-radius: 5px;
+    min-width: 400px;
+    height: 500px;
+    box-sizing: border-box;
+    background-image: 
+        linear-gradient(
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.6)
+        ),
+        url("../../assets/categories/biscuit.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
+.category-card-content {
+    padding: 10px;
+    height: 100%;
+    box-sizing: border-box;
+}
+.category-card-name {
+    position: relative;
+    top: 20%;
+    font-weight: bold;
+    font-size: 30pt;
+    margin: auto;
+    text-align: center;
+    color: #F5F5F5;
+}
+
+</style>
