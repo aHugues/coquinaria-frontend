5c77c7100892e3f9c4a4eeed37a2ae5b8579f2b3
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-06-23 20:40:01 +0200

    solve scrolling issue on recipes list

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,7 +7,7 @@
               </vertical-nav-bar>
             </div>
             <div class="recipes-category-wrapper" :class="{'full-page': verticalNavbarClosed}">
-              <recipes-list :category="category" :recipes="recipes"></recipes-list>
+              <recipes-list :category="category" :recipes="recipes" :menu-button="verticalNavbarClosed" @open-vertical-navbar="verticalNavbarClosed = false"></recipes-list>
             </div>
           <!-- </div> -->
         </div>
@@ -44,6 +44,8 @@ const recipesList = [
   { name: 'This is a super duper super recipe', duration: 125, description: 'This is a super duper description' },
   { name: 'This is a super super recipe', duration: 30, description: 'This is a super description' },
   { name: 'This is a super nice recipe', duration: 9, description: 'This is a nice description' },
+  { name: 'This could be a super super recipe', duration: 35, description: 'This could be a super description' },
+  { name: 'This could be a super nice recipe', duration: 90, description: 'This could be a nice description' },
 ];
 const imagesList = categoriesList.map(category => `/img/categories/${category.toLowerCase()}.png`);
 
--- src/components/Recipe/RecipesList.vue
+++ src/components/Recipe/RecipesList.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="recipes-list-wrapper">
-    <h2 class="recipes-list-title card">
-      {{ category }} recipes
-    </h2>
+    <div class="recipes-list-title-wrapper card" :class="{'full-size': menuButton}">
+      <transition name="fade">
+        <div v-if="menuButton" class="menu-button" @click="$emit('open-vertical-navbar')">
+          <vue-material-icon name="menu" :size="32"></vue-material-icon>
+        </div>
+      </transition>
+      <h2 class="recipes-list-title">{{ category }} recipes</h2>
+    </div>
     <div class="recipes-list">
       <div class="card recipe" v-for="(recipe, index) in recipes" :key="index">
         <div class="recipe-time">
@@ -22,6 +27,7 @@
 </template>
 
 <script>
+import VueMaterialIcon from 'vue-material-icon';
 
 function getColor(time) {
   if (time < 30) {
@@ -38,22 +44,51 @@ export default {
   props: {
     recipes: Array,
     category: String,
+    menuButton: Boolean,
   },
   methods: {
     getColor,
   },
+  components: {
+    VueMaterialIcon,
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
 .recipes-list-wrapper {
   padding: 10px;
+  height: 100%;
 }
 
 .recipes-list-title {
   margin: 0;
-  margin-bottom: 10px;
   font-weight: 600;
+  grid-column-start: 2;
+}
+
+.recipes-list-title-wrapper {
+  display: grid;
+  grid-template-columns: 0px auto;
+  grid-template-rows: 40px;
+  transition: grid-template-columns 0.3s;
+  align-items: center;
+  margin-bottom: 10px;
+  height: 8%;
+}
+
+.full-size {
+  grid-template-columns: 50px auto;
+  .menu-button {
+    cursor: pointer;
+  }
 }
 
 .circle {
@@ -73,6 +108,7 @@ export default {
   row-gap: 5px;
   column-gap: 5px;
   align-items: center;
+  width: 100%;
 }
 
 .recipe-title {
@@ -91,10 +127,13 @@ export default {
 }
 
 .recipes-list {
-  overflow: auto;
+  overflow-x: auto;
   display: grid;
   grid-template-columns: auto;
   row-gap: 10px;
+  z-index: 90;
+  // width: 100%;
+  height: 88%;
 }
 
 .card {
@@ -102,6 +141,9 @@ export default {
   background-color: #F5F5F5;
   box-sizing: border-box;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  &.recipe {
+    z-index: 95;
+  }
 }
 
 </style>
